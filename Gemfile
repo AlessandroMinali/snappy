ed6df5341378c977a25c33974d7c449e22ce8246
AlessandroMinali <alessandro.minali@gmail.com>
2017-08-29 20:39:48 -0400

    :cop:

--- Gemfile
+++ Gemfile
@@ -1,9 +1,11 @@
 source 'https://rubygems.org'
-ruby "2.3.0"
-gem 'sinatra'
-gem 'json'
+ruby '2.3.0'
 gem 'data_mapper'
-gem 'dm-sqlite-adapter', :group => :development
-gem 'dm-postgres-adapter', :group => :production
+gem 'dm-postgres-adapter', group: :production
+gem 'dm-sqlite-adapter', group: :development
+gem 'json'
 gem 'phantomjs'
+gem 'pry'
+gem 'rubocop'
+gem 'sinatra'
 gem 'thin'
--- snappy.rb
+++ snappy.rb
@@ -3,16 +3,20 @@ require 'sinatra'
 require 'uri'
 require 'data_mapper'
 require 'json'
+require 'pry'
+# rubocop:disable LineLength
+# rubocop:disable Metrics/BlockLength
 
-DataMapper::setup(:default, ENV['HEROKU_POSTGRESQL_TEAL_URL'] || "sqlite3://#{Dir.pwd}/gallery.db")
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_TEAL_URL'] ||
+                           "sqlite3://#{Dir.pwd}/gallery.db")
 
+# Schema for Snapshot model
 class Snapshot
   include DataMapper::Resource
   property :id, Serial
-  property :name, Text, :required => true
+  property :name, Text, required: true
   property :created_at, DateTime
   property :updated_at, DateTime
-
   validates_uniqueness_of :name
 end
 
@@ -22,7 +26,7 @@ helpers do
   include Rack::Utils
   alias_method :h, :escape_html
 
-  def store image
+  def store(image)
     s = Snapshot.new
     s.name = image
     s.created_at = Time.now
@@ -30,70 +34,61 @@ helpers do
     s.save
   end
 
-  def cached? image
-    s = Snapshot.first(:name => image)
-    if s
-      if s.updated_at.to_time < Time.now - 1 * 60
-
-        snap @image, params[:url], '1440', '900'
-
-        snap '2' + @image, params[:url], '640', '960', true
-
-        s.updated_at = Time.now
-        s.save
-      end
+  def cached?(image)
+    screen_shot = Snapshot.first(name: image)
+    if screen_shot
+      refresh_snap(screen_shot)
       true
+    else
+      false
     end
   end
 
-  def snap name, url, x, y, mobile=nil
-    mobile.nil? ? agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:35.0) Gecko/20100101 Firefox/35.0' : agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_1_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B440 Safari/600.1.4'
-    Phantomjs.run(
-      './public/script/peek.js',
-      url,
-      x,
-      y,
-      "./public/#{name}.png",
-      agent,
-    ) { |k|
-        p k
-        if k.chomp.include? 'timeout'
-          redirect to('/timeout')
-          return
-        end
-        store @image if k.chomp.include? 'done'
-    }
+  def refresh_snap(image)
+    return unless image.updated_at.to_time < Time.now - 60
+    snap image, params[:url], '1440', '900'
+    snap '2' + image, params[:url], '640', '960', true
+    image.updated_at = Time.now
+    image.save
+  end
+
+  def snap(name, url, x, y, mobile = nil)
+    agent = mobile ? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:35.0) Gecko/20100101 Firefox/35.0' : 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_1_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B440 Safari/600.1.4'
+    Phantomjs.run('./public/script/peek.js', url, x, y, "./public/#{name}.png", agent) do |k|
+      return redirect to('/timeout') if k.chomp.include? 'timeout'
+      store @image if k.chomp.include? 'done'
+    end
   end
 end
 
 after do
-  s = Snapshot.all :created_at.lt => Time.now - 2 * 60 * 60 * 24 * 7
-  JSON.parse(s.to_json).each { |s|
-    File.delete "./public/#{s['name']}.png"
+  images = Snapshot.all :created_at.lt => Time.now - 1_209_600
+  JSON.parse(images.to_json).each do |image|
+    File.delete "./public/#{image['name']}.png"
     begin
-      File.delete "./public/2#{s['name']}.png"
+      File.delete "./public/2#{image['name']}.png"
     rescue
       'file not found'
     end
-  }
-  s.destroy
+  end
+  image.destroy
 end
 
 before '/' do
   next unless request.post?
-  redirect to('/'), 'whoops!' unless params['url'] =~ /\A#{URI::regexp(['http', 'https'])}\z/
+  redirect to('/'), 'whoops!' unless params['url'] =~ /\A#{URI.regexp(%w[http https])}\z/
 end
 
 get '/' do
-  @snaps = Snapshot.all :limit => 6, :order => :updated_at.desc
+  @snaps = Snapshot.all limit: 6, order: :updated_at.desc
   erb :index
 end
 
 post '/snap' do
-  @image = params['url'].split("//")[-1]
+  @image = params['url'].split('//')[-1]
 
   # redirect to("/#{@image}") unless request.xhr?
-  if cached? @image 
+  if cached? @image
     redirect to("/#{@image}")
     return
   end
@@ -134,15 +129,16 @@ end
 
 get '/api/:url/?:format?' do
   content_type :json
-  @image = params[:url].split("//")[-1]
+  @image = params[:url].split('//')[-1]
 
-  params[:url] = 'http://' + params[:url] if params[:url].split("//").length < 2
+  params[:url] = 'http://' + params[:url] if params[:url].split('//').length < 2
 
   if cached? @image
-    return {url: "#{params[:url]}", desktop: "#{request.host}/desktop/#{@image}.png", mobile: "#{request.host}/mobile/#{@image}.png"}.to_json
+    return { url: params[:url].to_s, desktop: "#{request.host}/desktop/#{@image}.png", mobile: "#{request.host}/mobile/#{@image}.png" }.to_json
   end
 
-  @d, @m = nil, nil
+  @d = nil
+  @m = nil
 
   if params[:format].nil? || params[:format] == 'desktop'
     snap @image, params[:url], '1440', '900'
@@ -154,7 +150,7 @@ get '/api/:url/?:format?' do
     @m = "/mobile/#{@image}.png"
   end
 
-  {url: "#{params[:url]}", desktop: "#{request.host + @d unless @d.nil?}", mobile: "#{request.host + @m unless @m.nil?}"}.to_json
+  { url: params[:url].to_s, desktop: (request.host + @d unless @d.nil?).to_s, mobile: (request.host + @m unless @m.nil?).to_s }.to_json
 end
 
 not_found do
@@ -165,14 +161,14 @@ error do
   halt 500, 'Sorry there was a nasty error - ' + env['sinatra.error'].message
 end
 
-##TODO
+# #TODO
 # async phantomjs
 
-#stripe checkout and api key give
-#api access upon authorization
-#privacy of searched websites
+# stripe checkout and api key give
+# api access upon authorization
+# privacy of searched websites
 
-#javascript loading page | parallel image grab possibly?
+# javascript loading page | parallel image grab possibly?
 
-#deploy on heroku
-#deploy on DO
+# deploy on heroku
+# deploy on DO
